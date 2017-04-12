e43fafb239d1eff8f78a8c04c6938fa3663f8dba
Peter <geleridderacer>
2017-04-12 20:38:13 +0200

    strings

--- 01_variables/strings.py
+++ 01_variables/strings.py
@@ -4,5 +4,6 @@ print(game)
 print(game[0])  # zero based index
 print(game[1:3])  # 3 not included
 print(game[4:])
+print(game[:4])
+print(game[4:] + game[:4])
 print(game + ' is a game')  # string concat
-
