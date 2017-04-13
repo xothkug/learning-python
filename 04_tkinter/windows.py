9a621e2f6cf679495037b85ac12a08b4bf077094
Peter <geleridderacer>
2017-04-13 12:46:54 +0200

    tkinter

--- 04_tkinter/windows.py
+++ 04_tkinter/windows.py
@@ -2,8 +2,10 @@ from tkinter import *
 
 window = Tk()
 
+
 def button_on_click():
     print('ok')
+
 
 button = Button(window, text='dd', command=button_on_click)
 button.pack()
@@ -11,7 +13,7 @@ button.pack()
 canvas = Canvas(window, width=500, height=350)
 canvas.pack()
 
-rect = canvas.create_rectangle(50,50,100,100)
+rect = canvas.create_rectangle(50, 50, 100, 100)
 window.update()
 
 window.mainloop()
