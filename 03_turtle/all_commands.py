93f80f830dd2839cc3a6eeb722d17d7532f395aa
Peter <geleridderacer>
2017-04-13 09:44:03 +0200

    testing turtle commands

--- 03_turtle/all_commands.py
+++ 03_turtle/all_commands.py
@@ -1,9 +1,47 @@
 import turtle
 
-turtle.speed(0)
+def onClick(x,y):
+    turtle.fd(10)
 
-for i in range(4):
-    turtle.forward(100)
-    turtle.right(90)
+def vierkant():
+    for i in range(4):
+        turtle.forward(20)
+        turtle.right(90)
+    turtle.penup()
+    turtle.forward(30)
+    turtle.pendown()
+
+turtle.speed(0)
+turtle.shape("turtle")
+turtle.bgcolor("orange")
+
+turtle.home()
+turtle.penup()
+turtle.setx(-200)
+turtle.pendown()
+vierkant()
+
+turtle.pensize(2)
+vierkant()
+
+turtle.write("tekst")
+
+turtle.pensize(1)
+turtle.pencolor((1, 0, 0))
+vierkant()
+
+turtle.write("tekst")
+
+turtle.fillcolor("violet")
+turtle.begin_fill()
+vierkant()
+turtle.end_fill()
+
+turtle.circle(10)
+turtle.circle(120, 180)
+
+turtle.write("tekst")
+
+turtle.onclick(onClick)
 
 turtle.done()
