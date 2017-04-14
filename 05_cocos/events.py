f61b802f11c75b7d256fac43d263512494b850a2
Peter <geleridderacer>
2017-04-14 11:31:32 +0200

    pyglet

--- 05_cocos/events.py
+++ 05_cocos/events.py
@@ -1,5 +1,6 @@
 import cocos
 from cocos.actions import *
+import cocos.actions.grid3d_actions
 
 
 class Game(cocos.layer.ColorLayer):
@@ -8,15 +9,17 @@ class Game(cocos.layer.ColorLayer):
     def __init__(self):
         super(Game, self).__init__(64, 64, 224, 255)
 
-        sprite = cocos.sprite.Sprite('assets/spaceship.png')
-        sprite.position = 320, 240
-        sprite.scale = 1
-        self.add(sprite, z=1)
+        self.sprite = cocos.sprite.Sprite('assets/spaceship.png')
+        self.sprite.position = 320, 240
+        self.sprite.scale = 1
+        self.add(self.sprite, z=1)
         self.keys_pressed = set()
 
     def on_key_press(self, key, modifiers):
         self.keys_pressed.add(key)
         self.update_text()
+
+        self.sprite.do(grid3d_actions.Twirl(grid=(32, 24), duration=2))
 
     def on_key_release(self, key, modifiers):
         self.keys_pressed.remove(key)
@@ -24,6 +27,7 @@ class Game(cocos.layer.ColorLayer):
 
     def update_text(self):
         print(self.keys_pressed)
+
 
 cocos.director.director.init()
 cocos.director.director.run(cocos.scene.Scene(Game()))
